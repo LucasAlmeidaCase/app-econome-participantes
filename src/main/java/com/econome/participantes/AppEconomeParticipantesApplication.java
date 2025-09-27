15cd8e9b811ef8e5443921629bb3f5bf7cc83969
Lucas Almeida <lucas@uaicontrole.com.br>
2025-09-26 21:16:27 -0300

    Inicializa a aplicação Spring Boot com configuração básica

---
+++ src/main/java/com/econome/participantes/AppEconomeParticipantesApplication.java
@@ -0,0 +1,13 @@
+package com.econome.participantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppEconomeParticipantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppEconomeParticipantesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/econome/participantes/AppEconomeParticipantesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.econome.participantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppEconomeParticipantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
